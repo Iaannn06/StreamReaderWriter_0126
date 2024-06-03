ef78ad310d1a064d577f446ad103ef0fb8c0a136
Iaannn06 <alfianazad05@gmail.com>
2024-06-03 09:43:51 +0700

    membuka file dalam mode menulis

--- StreamReaderWriter_0126/StreamReaderWriter_0126.cpp
+++ StreamReaderWriter_0126/StreamReaderWriter_0126.cpp
@@ -1,8 +1,12 @@
-
+#include <fstream>
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	string baris;
+	ofstream outfile;
+
 }
+
 
